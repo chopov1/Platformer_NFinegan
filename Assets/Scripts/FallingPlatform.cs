1117a159b5ddba1e3f6bf2c58a01c0dca4db7584
Nicole Finegan <nicolefinegan@yahoo.com>
2023-03-22 20:51:53 -0500

    Falling Platform
    -Platform falls when player collides
    -Resetting the platform still needs debugging

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Code help from Stuart Spence (Youtube)
 
 public class FallingPlatform : MonoBehaviour
 {
+    //Variable that delays how long it takes between player and platform collision
     [SerializeField] float fallTimer = 1f;
-    Rigidbody rb;
-    
+    //Variable that resets the platform after some time
+    [SerializeField] float resetPlatform;
 
+    //The starting position of the falling platform
+    private Vector3 startingPosition;
+    Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startingPosition = transform.position;
         rb.isKinematic = true;
+    }
+
+    void Fall()
+    {
+        rb.isKinematic = false;
+        //call reset method here
+    }
+
+    private void Reset()
+    {
+        //reset platform code goes here
     }
 
     void OnCollisionEnter(Collision other)
@@ -19,11 +36,9 @@ public class FallingPlatform : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Invoke("Fall", fallTimer);
+            
         }
     }
 
-    void Fall()
-    {
-        rb.isKinematic = false;
-    }
+
 }
