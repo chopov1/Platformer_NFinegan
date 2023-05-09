691bf03ca1e1ed4e678ac7ab48076e6f10c7fad0
Nicole Finegan <nicolefinegan@yahoo.com>
2023-05-09 16:38:30 -0500

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,7 +35,6 @@ public class PlayerController : MonoBehaviour
     //Audio Source for jump sound
     public AudioSource jumpSound;
 
-
     // Start is called before the first frame update
     void Start()
     {
